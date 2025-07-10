4011c48818e1e2cba1c36841c5d36497439ed2b8
zai-server <zailabzayla@gmail.com>
2025-07-10 20:50:59 +0700

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -1,1 +1,28 @@
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const path = require('path');
 
+module.exports = {
+  name: 'downloader',
+  description: 'Unduh video dari YouTube',
+  async execute(client, message, arg) {
+    if (!arg || !arg.includes('youtube')) {
+      return client.sendMessage(message.from, '❗ Format: .downloader [link youtube]');
+    }
+
+    const info = await ytdl.getInfo(arg);
+    const fileName = path.resolve(__dirname, `../temp/${Date.now()}.mp4`);
+
+    client.sendMessage(message.from, '⏳ Mendownload...');
+
+    ytdl(arg, { quality: 'lowest' })
+      .pipe(fs.createWriteStream(fileName))
+      .on('finish', async () => {
+        await client.sendMessage(message.from, fs.readFileSync(fileName), {
+          sendMediaAsDocument: true,
+          caption: `🎞️ ${info.videoDetails.title}`
+        });
+        fs.unlinkSync(fileName);
+      });
+  }
+};
