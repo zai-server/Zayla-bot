a0f0c42d347a74a152f941466be62417e5218edf
zai-server <zailabzayla@gmail.com>
2025-07-10 20:55:40 +0700

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,1 +1,16 @@
+const users = {};
 
+module.exports = {
+  getOrCreateUser(id) {
+    if (!users[id]) {
+      users[id] = { zcoin: 100, quizPoints: 0 };
+    }
+    return users[id];
+  },
+  addZcoin(id, amount) {
+    this.getOrCreateUser(id).zcoin += amount;
+  },
+  addQuizPoint(id, points) {
+    this.getOrCreateUser(id).quizPoints += points;
+  }
+};
