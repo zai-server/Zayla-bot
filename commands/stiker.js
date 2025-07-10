66e9a3969fc86c05f296f8b3908e1cff80a84f14
zai-server <zailabzayla@gmail.com>
2025-07-10 20:51:34 +0700

    Update stiker.js

--- commands/stiker.js
+++ commands/stiker.js
@@ -1,1 +1,12 @@
+module.exports = {
+  name: 'sticker',
+  description: 'Ubah gambar jadi stiker',
+  async execute(client, message) {
+    if (!message.hasMedia) {
+      return client.sendMessage(message.from, '❗ Balas gambar dengan .sticker');
+    }
 
+    const media = await message.downloadMedia();
+    await client.sendMessage(message.from, media, { sendMediaAsSticker: true });
+  }
+};
