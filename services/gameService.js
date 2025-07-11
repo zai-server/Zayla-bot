16c6f099059ab4b6b3ee5297c08bd7bf2537cdc2
zai-server <zailabzayla@gmail.com>
2025-07-11 07:15:23 +0700

    Update gameService.js

--- services/gameService.js
+++ services/gameService.js
@@ -1,1 +1,20 @@
+module.exports = {
+  randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  rockPaperScissors(choice) {
+    const options = ['batu', 'gunting', 'kertas'];
+    const botChoice = options[Math.floor(Math.random() * 3)];
 
+    if (choice === botChoice) return { result: 'seri', botChoice };
+    if (
+      (choice === 'batu' && botChoice === 'gunting') ||
+      (choice === 'gunting' && botChoice === 'kertas') ||
+      (choice === 'kertas' && botChoice === 'batu')
+    ) {
+      return { result: 'menang', botChoice };
+    }
+
+    return { result: 'kalah', botChoice };
+  }
+};
