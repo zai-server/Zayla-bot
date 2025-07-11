d7604f694f14b880a04218cfac649a72303dac5e
zai-server <zailabzayla@gmail.com>
2025-07-11 07:07:33 +0700

    Update paymentService.js

--- services/paymentService.js
+++ services/paymentService.js
@@ -1,1 +1,15 @@
+const userService = require('./userService');
 
+module.exports = {
+  chargeUser(id, amount) {
+    const user = userService.getOrCreateUser(id);
+    if (user.zcoin < amount) {
+      return false;
+    }
+    user.zcoin -= amount;
+    return true;
+  },
+  addFunds(id, amount) {
+    userService.addZcoin(id, amount);
+  }
+};
