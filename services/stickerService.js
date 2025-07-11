8fb12b81f7894bcf6ee6ab492f7c6f5c94398d25
zai-server <zailabzayla@gmail.com>
2025-07-11 07:14:49 +0700

    Update stickerService.js

--- services/stickerService.js
+++ services/stickerService.js
@@ -1,1 +1,9 @@
+const stickerService = require('../services/stickerService');
 
+module.exports = {
+  name: 'stiker',
+  description: 'Buat stiker dari gambar',
+  async execute(client, message) {
+    await stickerService.createStickerFromMedia(client, message);
+  }
+};
