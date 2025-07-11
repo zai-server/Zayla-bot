3e32304c47b73b4eca5d05965ab0261022c4679e
zai-server <zailabzayla@gmail.com>
2025-07-11 07:20:38 +0700

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -1,1 +1,18 @@
+const userService = require('../services/userService');
 
+module.exports = {
+  name: 'user',
+  description: 'Cek data user lain atau dirimu sendiri',
+  async execute(client, message, arg) {
+    const target = arg || message.from;
+    const user = userService.getOrCreateUser(target);
+
+    const text = `
+🧑 *Data User*
+🪪 ID: ${target}
+💰 Zcoin: ${user.zcoin}
+📘 Poin Kuis: ${user.quizPoints}
+    `;
+    client.sendMessage(message.from, text);
+  }
+};
