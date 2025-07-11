b5c5f86fa73c8808b0d9f001066fdbf9baec5a61
zai-server <zailabzayla@gmail.com>
2025-07-11 07:03:06 +0700

    Update authentication.js

--- handlers/authentication.js
+++ handlers/authentication.js
@@ -1,1 +1,12 @@
+const allowedUsers = [];
 
+module.exports = {
+  isAuthorized(id) {
+    return allowedUsers.includes(id);
+  },
+  addUser(id) {
+    if (!allowedUsers.includes(id)) {
+      allowedUsers.push(id);
+    }
+  }
+};
