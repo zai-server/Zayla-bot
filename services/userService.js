3c518ff581465b1055744604257e07d700854905
zai-server <zailabzayla@gmail.com>
2025-07-11 07:26:53 +0700

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,16 +1,46 @@
-const users = {};
+const fs = require('fs');
+const path = require('path');
+const dataFile = path.join(__dirname, '../data/users.json');
+
+let users = {};
+
+// Load data saat start
+if (fs.existsSync(dataFile)) {
+  users = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
+} else {
+  fs.writeFileSync(dataFile, JSON.stringify(users, null, 2));
+}
+
+// Simpan data setiap ada perubahan
+function saveData() {
+  fs.writeFileSync(dataFile, JSON.stringify(users, null, 2));
+}
 
 module.exports = {
   getOrCreateUser(id) {
     if (!users[id]) {
       users[id] = { zcoin: 100, quizPoints: 0 };
+      saveData();
     }
     return users[id];
   },
   addZcoin(id, amount) {
-    this.getOrCreateUser(id).zcoin += amount;
+    const user = this.getOrCreateUser(id);
+    user.zcoin += amount;
+    saveData();
+  },
+  deductZcoin(id, amount) {
+    const user = this.getOrCreateUser(id);
+    if (user.zcoin >= amount) {
+      user.zcoin -= amount;
+      saveData();
+      return true;
+    }
+    return false;
   },
   addQuizPoint(id, points) {
-    this.getOrCreateUser(id).quizPoints += points;
+    const user = this.getOrCreateUser(id);
+    user.quizPoints += points;
+    saveData();
   }
 };
